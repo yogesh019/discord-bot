2dc7c0be4e47b83122ed83a07f52d7376570df76
yogesh <yogesh.sharma@grabpay.com>
2020-10-08 19:37:35 +0530

    [venev update]

--- env/bin/activate.fish
+++ env/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/yogesh.sharma/Desktop/dummy/discordBot/env"
+set -gx VIRTUAL_ENV "/Users/yogesh.sharma/Desktop/dummy/discord-bot/env"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
